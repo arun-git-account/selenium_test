011275de65cba4934ffd315dbd4de081c1d02237
arunsharma <arunsharma@localhost>
2020-04-16 17:33:43 +0530

    Initial commit

---
+++ src/test/java/testNG/addbook.java
@@ -0,0 +1,31 @@
+package testNG;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class addbook {
+	  WebDriver driver;
+	@BeforeMethod
+	public void launch() {
+		System.setProperty("webdriver.chrome.driver","chromedriver");
+		  driver = new ChromeDriver();
+		  driver.get("http://3.15.14.24:8080/");
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	}
+  @Test
+  public void verifyaddbook() {
+	  driver.findElement(By.xpath("//a[text()='About Us']")).click();
+  }
+  
+  @AfterMethod
+  public void close() {
+	  driver.close();
+	}
+}
