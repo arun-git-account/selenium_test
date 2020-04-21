d53cc24744c8bafa66e7798c1cf5978dc0bb0c27
arun-git-account <61576995+arun-git-account@users.noreply.github.com>
2020-04-21 13:58:13 +0530

    Update addbook.java

--- src/test/java/testNG/addbook.java
+++ src/test/java/testNG/addbook.java
@@ -15,7 +15,7 @@ public class addbook {
 	public void launch() {
 		System.setProperty("webdriver.chrome.driver","chromedriver");
 		  driver = new ChromeDriver();
-		  driver.get("http://18.219.229.183/:8080/");
+		  driver.get("http://18.219.229.183:8080/");
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 	}
